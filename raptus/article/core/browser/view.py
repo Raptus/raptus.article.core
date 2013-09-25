87339da68653f1122d37b1ef90d0d97dc9783f64
Simon Käser <skaeser@raptus.com>
2013-09-25 10:11:11 +0200

    The article view now implements IViewView

--- raptus/article/core/browser/view.py
+++ raptus/article/core/browser/view.py
@@ -3,13 +3,15 @@ from zope.interface import implements
 from Products.Five.browser import BrowserView
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
+from plone.app.layout.globals.interfaces import IViewView
+
 from raptus.article.core import interfaces
 
 
 class View(BrowserView):
     """View of an article
     """
-    implements(interfaces.IArticleView)
+    implements(interfaces.IArticleView, IViewView)
     template = ViewPageTemplateFile('view.pt')
 
     def __call__(self):
